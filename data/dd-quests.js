77d221088c5e9145fb1f3c9ce15d2555dc5175b8
namiyukuta-cmd <namiyukuta@gmail.com>
2026-08-30 11:35:25 +0900

    Add quest catalog for scenario selection

---
+++ data/dd-quests.js
@@ -0,0 +1,9 @@
+window.DDQuests = [
+  {
+    id: 'frozen-offerings',
+    title: 'Frozen Offerings',
+    titleJa: 'Frozen Offerings',
+    source: 'dra34_FrozenOfferings(1).pdf',
+    dataSheet: 'Frozen Offerings_シナリオ整理'
+  }
+];
