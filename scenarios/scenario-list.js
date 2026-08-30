1c5f0a75668e8d213fbeb5b810b5ad12ab7e29c2
namiyukuta-cmd <namiyukuta@gmail.com>
2026-08-30 11:40:01 +0900

    Add lightweight scenario registry

---
+++ scenarios/scenario-list.js
@@ -0,0 +1,8 @@
+window.DDScenarioList = [
+  {
+    id: 'frozen-offerings',
+    title: 'Frozen Offerings',
+    titleJa: 'Frozen Offerings',
+    entry: 'scenarios/frozen-offerings/scenario.js'
+  }
+];
