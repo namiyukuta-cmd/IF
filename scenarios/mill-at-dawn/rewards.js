ef364f01db1bfdb6dc45ef6dde4d3b009c10b720
namiyukuta-cmd <namiyukuta@gmail.com>
2026-08-30 14:01:18 +0900

    Add Mill at Dawn rewards

---
+++ scenarios/mill-at-dawn/rewards.js
@@ -0,0 +1,5 @@
+window.DDScenarioData = window.DDScenarioData || {};
+window.DDScenarioData['mill-at-dawn'] = window.DDScenarioData['mill-at-dawn'] || {};
+window.DDScenarioData['mill-at-dawn'].rewards = [
+  { id:'REWARD_MILL_HEALING_POTION', itemId:'ITEM_MILL_HEALING_POTION', quantity:1, name:'Miraからの礼' }
+];
